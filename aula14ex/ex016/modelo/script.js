93a16f9bc280ba64999f7ab00f766f4ef79e3b40
Livia Boechat <108305176+LiviaBoechat@users.noreply.github.com>
2022-09-28 16:44:58 -0300

    fim modulo E

--- aula14ex/ex016/modelo/script.js
+++ aula14ex/ex016/modelo/script.js
@@ -1,29 +1,29 @@
 function contar() {
-    let ini = document.getElementById('txti') //referenciando os inputs 
+    let ini = document.getElementById('txti') //controles dos inputs 
     let fim = document.getElementById('txtf')
     let passo = document.getElementById('txtp')
     let res = document.getElementById('res')
     
     /*TESTANDO:
     if (ini.value.length == 0 || fim.value.length == 0 || passo.value.length == 0) {
-        window.alert('[ERRO] Falttam dados!')
+        window.alert('[ERRO] Faltam dados!')
     } else {
         alert('TUDO OK!')
     }
    */
 
-    if (ini.value.length == 0 || fim.value.length == 0 || passo.value.length == 0) {
+    if (ini.value.length == 0 || fim.value.length == 0 || passo.value.length == 0) { //length é comprimento (qts caracteres tem dentro)
         res.innerHTML = 'Impossível contar!'
     } else {
-        res.innerHTML = 'Contando:' //substitui o texto da div no HTML
-        let i = Number(ini.value) //pegando o valor(string) que será escrito no input e transformando em Number. dúvida: PQ não fez isso já na primeira let lá em cima qd referenciou os inputs?
+        res.innerHTML = 'Contando: <br>' //substitui o texto da div no HTML. O <br> quebra a linha
+        let i = Number(ini.value) //pegando o valor(string) que será escrito no input é transformando em Number. dúvida: PQ não fez isso já na primeira let lá em cima qd referenciou os inputs?
         let f = Number(fim.value)
         let p = Number(passo.value)
         
         if (p <= 0) {
             res.innerHTML = 'Passo inválido! Considerando PASSO 1:'
             p = 1 //passo (let p) passa automaticamente a valer 1
-        }
+        } //dúvida: pq não tem o else?
 
         //a repetição se dará da seguinte forma(for): o contador(let c) vai começar com o valor do início(let i); enquanto o contador for menor ou igual ao valor final(let p), o contador vai receber ELE MESMO(+=) mais o passo(let p)
 
--- aula14ex/ex017/modelo/script.js
+++ aula14ex/ex017/modelo/script.js
@@ -1,6 +1,6 @@
 function tabuada() {
-    let num = document.getElementById('txtn') //referenciando o input
-    let tab = document.getElementById('seltab') //referenciando o seletor
+    let num = document.getElementById('txtn') //controle do input
+    let tab = document.getElementById('seltab') //controle do seletor
 
     if (num.value.length == 0) {
        window.alert('Por favor, digite um número!') 
@@ -9,9 +9,9 @@ function tabuada() {
         tab.innerHTML = '' //para excluir a tabuada antiga qd entrar nova
         let c = 1 //contador para repetição usando while
         while (c <= 10) {
-            let item = document.createElement('option') //em vez de criar o option UM POR UM no selector no HTML e referenciar no JS, criei direto no JS. (ver como cria selector no HTML, smp tem value)
+            let item = document.createElement('option') //em vez de criar o option UM POR UM no selector no HTML e referenciar no JS, criei direto no JS. (ver como cria selector no HTML, smp tem option)
             item.text= `${n} x ${c} = ${n*c}`
-            tab.appendChild(item) //para aparecer o item no seletor
+            tab.appendChild(item) //para aparecer o let item no seletor. Isso pq ele não está ainda conectado ao seletor no HTML, e sim foi criado no JS direto
             c++ //acrescentar um multiplicador a cada repetição
         }
     }
